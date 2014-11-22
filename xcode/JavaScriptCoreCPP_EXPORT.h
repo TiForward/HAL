00fde93e2645816a27f166aea01cbd5cba6efca6
matt-langston <matt-langston@users.noreply.github.com>
2014-11-22 14:50:02 -0800

    Change Travis CI to use new build infrastructure, testing both JavaScriptCoreCPP, TitaniumKit and Util.

---
+++ xcode/JavaScriptCoreCPP_EXPORT.h
@@ -0,0 +1,41 @@
+
+#ifndef JAVASCRIPTCORECPP_EXPORT_H
+#define JAVASCRIPTCORECPP_EXPORT_H
+
+#ifdef JAVASCRIPTCORECPP_STATIC_DEFINE
+#  define JAVASCRIPTCORECPP_EXPORT
+#  define JAVASCRIPTCORECPP_NO_EXPORT
+#else
+#  ifndef JAVASCRIPTCORECPP_EXPORT
+#    ifdef JavaScriptCoreCPP_EXPORTS
+        /* We are building this library */
+#      define JAVASCRIPTCORECPP_EXPORT __attribute__((visibility("default")))
+#    else
+        /* We are using this library */
+#      define JAVASCRIPTCORECPP_EXPORT __attribute__((visibility("default")))
+#    endif
+#  endif
+
+#  ifndef JAVASCRIPTCORECPP_NO_EXPORT
+#    define JAVASCRIPTCORECPP_NO_EXPORT __attribute__((visibility("hidden")))
+#  endif
+#endif
+
+#ifndef JAVASCRIPTCORECPP_DEPRECATED
+#  define JAVASCRIPTCORECPP_DEPRECATED __attribute__ ((__deprecated__))
+#endif
+
+#ifndef JAVASCRIPTCORECPP_DEPRECATED_EXPORT
+#  define JAVASCRIPTCORECPP_DEPRECATED_EXPORT JAVASCRIPTCORECPP_EXPORT JAVASCRIPTCORECPP_DEPRECATED
+#endif
+
+#ifndef JAVASCRIPTCORECPP_DEPRECATED_NO_EXPORT
+#  define JAVASCRIPTCORECPP_DEPRECATED_NO_EXPORT JAVASCRIPTCORECPP_NO_EXPORT JAVASCRIPTCORECPP_DEPRECATED
+#endif
+
+#define DEFINE_NO_DEPRECATED 0
+#if DEFINE_NO_DEPRECATED
+# define JAVASCRIPTCORECPP_NO_DEPRECATED
+#endif
+
+#endif
