38d7c7cd268084a2ec578d8a3514ec5e97bbe8cc
matt-langston <matt-langston@users.noreply.github.com>
2014-11-30 16:15:57 -0800

    Export virtual_enable_shared_from_this from DLLs.

--- include/JavaScriptCoreCPP/detail/virtual_enable_shared_from_this_base.hpp
+++ include/JavaScriptCoreCPP/detail/virtual_enable_shared_from_this_base.hpp
@@ -27,13 +27,13 @@ namespace JavaScriptCoreCPP { namespace detail {
 //   using enable_shared_from_this<C>::shared_from_this;
 // }
 
-struct JAVASCRIPTCORECPP_EXPORT virtual_enable_shared_from_this_base : std::enable_shared_from_this<virtual_enable_shared_from_this_base> {
+struct JAVASCRIPTCORECPP_EXPORT virtual_enable_shared_from_this_base : public std::enable_shared_from_this<virtual_enable_shared_from_this_base> {
   virtual ~virtual_enable_shared_from_this_base() {
   }
 };
 
 template<typename T>
-struct virtual_enable_shared_from_this : virtual virtual_enable_shared_from_this_base {
+struct JAVASCRIPTCORECPP_EXPORT virtual_enable_shared_from_this : virtual public virtual_enable_shared_from_this_base {
   std::shared_ptr<T> shared_from_this() {
     return std::dynamic_pointer_cast<T>(virtual_enable_shared_from_this_base::shared_from_this());
   }
